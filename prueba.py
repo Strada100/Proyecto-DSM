3d55024fa651ff1c5f5e28d85515f66c9cbc59e2
Strada100 <viajero35138@innovaccion.mx>
2021-02-20 17:20:34 -0600

    De vuelta a la normalidad

--- prueba.py
+++ prueba.py
@@ -2,7 +2,7 @@ from cv2 import cv2
 import numpy as np
 Green=np.uint8([[[0,255,0]]])
 Orange=np.uint8([[[0,128,255]]])
-White=np.uint8([[[1,255,255]]])
+White=np.uint8([[[255,255,255]]])
 
 hsv_green=cv2.cvtColor(Green,cv2.COLOR_BGR2HSV)
 hsv_orange=cv2.cvtColor(Orange,cv2.COLOR_BGR2HSV)
